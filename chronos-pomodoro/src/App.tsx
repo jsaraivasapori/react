6fbd5b5473a594d146ac4b5d1d50a6c0d36413cb
João <joaosaraivasapori@gmail.com>
2025-09-19 13:02:29 -0300

    .

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/theme.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+import './styles/global.css';
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Teste</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis quos
+        dolor qui quis ipsum veniam nesciunt, officiis asperiores dolores nisi!
+        Necessitatibus, est asperiores impedit quia doloribus sapiente minus
+        aut. Voluptatum!
       </p>
     </>
-  )
+  );
 }
-
-export default App
