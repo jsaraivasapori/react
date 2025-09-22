83ca12180280b6aa67d28da38c95d4f11bb66c1b
João Vitor <joaosaraivasapori@gmail.com>
2025-09-22 14:45:49 -0300

    AULA 24

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -2,16 +2,17 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
-    return (
-        <>
-            <Container>
-                <Logo />
-            </Container>
-            <Container>
-                <section>Menu</section>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+    </>
+  );
 }
