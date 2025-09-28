f1b586586847c84bf569926aad6cd2f66230f718
João Vitor <joaosaraivasapori@gmail.com>
2025-09-28 15:58:13 -0300

    aula 41

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,62 +1,7 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { CountDown } from "./components/CountDown";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { PlayCircleIcon } from "lucide-react";
-import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <DefaultInput
-              labelText="oi"
-              id="meuInput"
-              type="text"
-              placeholder="Meu input"
-            />
-          </div>
-
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton
-              icon={
-                <>
-                  <PlayCircleIcon />
-                </>
-              }
-            />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer></Footer>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
