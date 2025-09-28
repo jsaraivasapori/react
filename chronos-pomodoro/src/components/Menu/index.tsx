8b8f22205e1b3de52a5f1443156d3e7243b090d1
João Vitor <joaosaraivasapori@gmail.com>
2025-09-28 14:40:52 -0300

    aula 36

--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,28 +1,54 @@
-import {
-  HistoryIcon,
-  HouseIcon,
-  SettingsIcon,
-  SunIcon,
-  TimerIcon,
-} from "lucide-react";
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState } from "react";
 
+type AvaliableThemes = "dark" | "light";
 export function Menu() {
+  const [theme, setTheme] = useState<AvaliableThemes>("dark");
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+    const newTheme = theme === "dark" ? "light" : "dark";
+    console.log(event);
+
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+  }
+
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ir para a Home"
+        title="Ir para a Home"
+      >
         <HouseIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ver histórico"
+        title="Ver histórico"
+      >
         <HistoryIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
-        <TimerIcon />
-      </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Configurações"
+        title="Configurações"
+      >
         <SettingsIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        className={styles.menuLink}
+        onClick={(event) => handleThemeChange(event)}
+        aria-label="Mudar tema"
+        title="Mudar tema"
+      >
         <SunIcon />
       </a>
     </nav>
