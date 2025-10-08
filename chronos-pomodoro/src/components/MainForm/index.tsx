d0a9a4b09689ae92d205fe95a4d527763ba1e18e
João Vitor <joaosaraivasapori@gmail.com>
2025-10-08 10:44:23 -0300

    aula 55

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -6,11 +6,15 @@ import styles from "./styles.module.css";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
 
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  //ciclos
+  const nextCycle = getNextCycle(state.currentCycle);
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -43,7 +47,7 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1, // Conferir
+        currentCycle: nextCycle, //sera o estado anterior pq so muda na proxima setState
         secondsRemaining, // Conferir
         formattedSecondsRemaining: "00:00", // Conferir
         tasks: [...prevState.tasks, newTask],
