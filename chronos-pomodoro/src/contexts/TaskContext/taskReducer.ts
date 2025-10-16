d512621796a7d82672d12e5ccad11e4b44541688
João <joaosaraivasapori@gmail.com>
2025-10-16 14:28:36 -0300

    aula  67

---
+++ chronos-pomodoro/src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,23 @@
+import type { TaskStateModel } from "../../models/TaskStateModel";
+import { type TaskActionModel, TaskActionTypes } from "./taskActions";
+
+export function taskReducer(
+    state: TaskStateModel,
+    action: TaskActionModel
+): TaskStateModel {
+    switch (action.type) {
+        case TaskActionTypes.START_TASK: {
+            return state;
+        }
+
+        case TaskActionTypes.INTERRUPT_TASK: {
+            return state;
+        }
+
+        case TaskActionTypes.RESET_STATE: {
+            return state;
+        }
+        default:
+            return state;
+    }
+}
